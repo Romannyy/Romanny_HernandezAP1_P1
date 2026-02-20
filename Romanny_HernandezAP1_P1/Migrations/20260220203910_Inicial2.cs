8490396cefcb3d3664ca037400f3545e62709116
Romanny Hernandez <romanny_hernandez@ucne.edu.do>
2026-02-20 18:45:52 -0400

    QuickGrid para tabla, agregar propiedad cantidad, agregar filtro de búsqueda por rango de fecha, actualizar base de datos.

---
+++ Romanny_HernandezAP1_P1/Migrations/20260220203910_Inicial2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Romanny_HernandezAP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Costo",
+                table: "EntradasHuacales",
+                newName: "Precio");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Cantidad",
+                table: "EntradasHuacales",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cantidad",
+                table: "EntradasHuacales");
+
+            migrationBuilder.RenameColumn(
+                name: "Precio",
+                table: "EntradasHuacales",
+                newName: "Costo");
+        }
+    }
+}
